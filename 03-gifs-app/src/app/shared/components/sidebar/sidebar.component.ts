b6213a5137e5368a61f0d16c907d92ccee1383e4
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2023-10-02 20:22:50 -0500

    Creación del diseño de la página por medio de componentes y servicios, se deja funcionando el buscador solo con ingreso de información y guardado de la misma en el sidebar

---
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  public _tagsHistory: string[] = []
+
+  constructor(private gifsService: GifsService) { }
+
+  get tagsHistory(): string[] {
+    return this.gifsService.tagsHistory
+  }
+}
