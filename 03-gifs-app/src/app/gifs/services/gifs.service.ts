8e1d292a8ec483eac6e42c657d62242e9bfcf5a6
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2023-10-04 21:53:04 -0500

    Se agregan estilos y loader al cargar una imagen

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,9 @@ export class GifsService {
   public gifList: Gif[] = []
   private _tagsHistory: string[] = []
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage()
+  }
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory]
@@ -45,5 +47,21 @@ export class GifsService {
 
     this._tagsHistory.unshift(tag)
     this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this.saveLocalStorage()
+  }
+
+  // Guardar la lista o el historial de gifs en el localstorage
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+  }
+
+  // Leer la información almacenada en el localstorage
+  private loadLocalStorage(): void {
+    if(!localStorage.getItem('history')) return
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!)
+
+    if(this._tagsHistory.length === 0) return
+    this.searchTag(this._tagsHistory[0])
   }
 }
