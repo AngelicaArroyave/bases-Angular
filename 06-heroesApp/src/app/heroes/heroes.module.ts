19d4f71f33262a4f2b75ef73eba66904f536a9a9
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2023-11-26 16:12:18 -0500

    Manejo de información y estructura html para editar y agregar nuevos héroes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -12,15 +12,17 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
+    CardComponent,
+    ConfirmDialogComponent,
     HeroPageComponent,
     LayoutPageComponent,
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
-    CardComponent,
 
     // Pipe personalizado
     HeroImagePipe
--- 06-heroesApp/src/app/heroes/services/heroes.service.ts
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
 
@@ -24,5 +24,23 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${BASE_URL}/heroes?q=${query}&_limit=6`)
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${BASE_URL}/heroes`, hero)
+  }
+
+  // PATCH: Actualiza parcialmente la información de un héroe
+  updateHero(hero: Hero): Observable<Hero> {
+    if(!hero.id) throw Error('Hero id is required')
+    return this.http.patch<Hero>(`${BASE_URL}/heroes/${hero.id}`, hero)
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete(`${BASE_URL}/heroes/${id}`)
+            .pipe(
+              map(response => true), // Si no hay un error devuelve true
+              catchError(error => of(false)) // Si hay un error devuelve false
+            )
   }
 }
