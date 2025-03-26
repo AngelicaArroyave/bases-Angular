30ab98e272aadaa05c31ee5bee999a8bb2bfd9ef
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2025-03-26 10:45:12 -0500

    Expansión de bases en Angular

--- 02-bases-2025/src/app/app.routes.ts
+++ 02-bases-2025/src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { DragonballSuperPageComponent } from './pages/dragonball-super/dragonball-super-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
 import { Routes } from '@angular/router';
 
@@ -10,5 +12,17 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent
+  },
+  {
+    path: 'dragonball',
+    component: DragonballPageComponent
+  },
+  {
+    path: 'dragonball-super',
+    component: DragonballSuperPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
