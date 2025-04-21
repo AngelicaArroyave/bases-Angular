71aadd7ba4375588f19e0cd22d5fb4373f9ae023
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2025-04-21 10:01:55 -0500

    Pipes propios de Angular: AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe

--- 05-pipes-app-2025/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app-2025/src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+
+import { AvailableLocales, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe,
+    DatePipe
+  ],
   templateUrl: './basic-page.component.html'
 })
-export default class BasicPageComponent { }
+export default class BasicPageComponent {
+  localeService = inject(LocaleService)
+  currentLocale = signal(inject(LOCALE_ID))
+
+  nameLower = signal('angie')
+  nameUpper = signal('ANGIE')
+  fullName = signal('aNgIe AriAS')
+
+  customDate = signal(new Date())
+
+  tickingDateEffect = effect(() => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+    }, 1000)
+    return () => clearInterval(interval)
+  })
+
+  changeLocale(locale: AvailableLocales) {
+    this.localeService.changeLocale(locale)
+  }
+}
